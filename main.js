25ff206f902fc39bfe8b48f6c7b522c1891aca60
Adéla Krásová <>
2022-03-03 19:10:51 +0100

    init commit + panacek walks

--- main.js
+++ main.js
@@ -7,3 +7,45 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+/* function panacekDoprava(udalost) {
+	let panacek = document.getElementById("panacek");
+	console.log(udalost);
+	panacek.style.left = "100px";
+}; */
+let panacek = document.getElementById("panacek");
+
+
+	window.addEventListener("load", ()=> {
+		panacek.style.position = "absolute";
+		panacek.style.left=10+"px";
+		panacek.style.top=10+"px";
+	});
+
+window.addEventListener("keydown", (e) => {
+	
+		switch (e.key) {
+			
+	 		case "ArrowLeft":
+				if (parseInt(panacek.style.left) >= 10 ) {panacek.style.left = parseInt(panacek.style.left) - 7 + "px";
+				break;} 
+				else {console.log("Tam nelze jít")}
+			
+				case "ArrowRight":
+					if (parseInt(panacek.style.right) >= 10 ) {panacek.style.left = parseInt(panacek.style.left) + 7 + "px";
+				break;}
+				else {console.log("Tam nelze jít")}
+			}
+
+	if (parseInt(panacek.style.top) >= 10 ) {
+			switch (e.key) {				
+			case "ArrowUp":
+				panacek.style.top = parseInt(panacek.style.top) - 7 + "px";
+				break;
+			case "ArrowDown":
+				panacek.style.top = parseInt(panacek.style.top) + 7 + "px";
+				break};
+			} else {console.log("Tam nelze jít")};
+			
+		
+});
